8b7e11bc93a644f78c797064b064bca8563685dd
Mael2001 <Mariof2001@unitec.edu>
2021-02-09 19:06:23 -0600

    Commit base

---
+++ plugin_main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <easm.h>
+#include <rlutil.h>
+
+extern "C" ErrorCode handleSyscall(uint32_t *regs, void *mem, MemoryMap *mem_map)
+{
+    unsigned v0 = regs[Register::v0];
+
+    switch (v0)
+    {
+        case 20:
+        {
+            int a0 = regs[Register::a0];
+            int a1 = regs[Register::a1];
+            regs[Register::v0] = a0 + a1;
+            return ErrorCode::Ok;
+        }
+        case 21:
+        {
+            rlutil::setColor(1);
+        }
+        case 22:
+        {
+
+        }
+        case 23:
+        {
+
+        }
+        default:
+            if (v0 > 20 && v0 <= 50)
+            {
+                std::cout << "Syscall: " << v0 << '\n' << std::flush;
+                return ErrorCode::Ok;
+            }
+            else
+            {
+                return ErrorCode::SyscallNotImplemented;
+            }
+    }
+}
